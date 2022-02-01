e29339162f01f3ab475a567bef3c312583a1a4fe
IlyaBorisovDly <76851543+IlyaBorisovDly@users.noreply.github.com>
2022-02-01 18:12:46 +0300

    Made categories fragment visual representation + improved sample data for better vision

--- app/src/main/java/com/example/gamepicker/presentation/categories/CategoriesFragment.kt
+++ app/src/main/java/com/example/gamepicker/presentation/categories/CategoriesFragment.kt
@@ -1,32 +1,19 @@
 package com.example.gamepicker.presentation.categories
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.gamepicker.R
+import com.example.gamepicker.databinding.FragmentCategoriesBinding
 
 class CategoriesFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = CategoriesFragment()
-    }
-
-    private lateinit var viewModel: CategoriesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+    ): View {
+        val binding = FragmentCategoriesBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CategoriesViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/example/gamepicker/presentation/categories/CategoriesViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gamepicker.presentation.categories
-
-import androidx.lifecycle.ViewModel
-
-class CategoriesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
