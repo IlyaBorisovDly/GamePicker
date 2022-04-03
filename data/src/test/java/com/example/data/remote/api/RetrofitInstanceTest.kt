cdfe2deb259f5055cce82dc42482f85102abf363
Naqswell <naqswell@gmail.com>
2022-04-03 21:26:53 +0300

    unit tests for Retrofit responses

--- data/src/test/java/com/example/data/remote/api/RetrofitInstanceTest.kt
+++ data/src/test/java/com/example/data/remote/api/RetrofitInstanceTest.kt
@@ -5,12 +5,57 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 class RetrofitInstanceTest {
-    val retrofit = RetrofitInstance
+    @Test
+    fun `RawgApi games GET test`() {
+        val response = runBlocking { RetrofitInstance.api.games(1) }
+        TestCase.assertTrue(response.code() == 200)
+    }
 
     @Test
-    fun `retrofit test`() {
-        val response = runBlocking { RetrofitInstance.api.creators(1, null) }
+    fun `RawgApi game GET test`() {
+        val response = runBlocking { RetrofitInstance.api.game(1) }
+        TestCase.assertTrue(response.code() == 200)
+    }
 
+    @Test
+    fun `RawgApi gameDetails GET test`() {
+        val response = runBlocking { RetrofitInstance.api.gameDetails(1) }
+        TestCase.assertTrue(response.code() == 200)
+    }
+
+    @Test
+    fun `RawgApi gameScreenshots GET test`() {
+        val response = runBlocking { RetrofitInstance.api.gameScreenshots(1) }
+        TestCase.assertTrue(response.code() == 200)
+    }
+
+    @Test
+    fun `RawgApi creators GET test`() {
+        val response = runBlocking { RetrofitInstance.api.creators() }
+        TestCase.assertTrue(response.code() == 200)
+    }
+
+    @Test
+    fun `RawgApi publishers GET test`() {
+        val response = runBlocking { RetrofitInstance.api.publishers() }
+        TestCase.assertTrue(response.code() == 200)
+    }
+
+    @Test
+    fun `RawgApi developers GET test`() {
+        val response = runBlocking { RetrofitInstance.api.developers() }
+        TestCase.assertTrue(response.code() == 200)
+    }
+
+    @Test
+    fun `RawgApi genres GET test`() {
+        val response = runBlocking { RetrofitInstance.api.genres() }
+        TestCase.assertTrue(response.code() == 200)
+    }
+
+    @Test
+    fun `RawgApi platforms GET test`() {
+        val response = runBlocking { RetrofitInstance.api.platforms() }
         TestCase.assertTrue(response.code() == 200)
     }
 }
