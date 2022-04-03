e5e9f6dd547c81e16c94b93a46fdd379f0a89394
Naqswell <naqswell@gmail.com>
2022-04-03 20:12:43 +0300

    gradle test coroutines

---
+++ data/src/test/java/com/example/data/repositories/DeveloperRepositoryImplTest.kt
@@ -0,0 +1,26 @@
+package com.example.data.repositories
+
+import com.example.data.remote.sources.RemoteDataSourceImpl
+import com.example.domain.entities.states.Status
+import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class DeveloperRepositoryImplTest {
+    private val remoteDataSource = RemoteDataSourceImpl()
+
+    @Test
+    fun `getDeveloper success status`() {
+        when (val list = runBlocking { remoteDataSource.getDevelopers() }) {
+            is Status.Success -> {
+                TestCase.assertNotNull(list.data)
+                println(list.data)
+            }
+            is Status.Failure -> {
+                TestCase.assertNotNull(list.message)
+                println(list.message)
+            }
+            is Status.Loading -> {}
+        }
+    }
+}
---
+++ data/src/test/java/com/example/data/repositories/GameDetailsRepositoryImplTest.kt
@@ -0,0 +1,26 @@
+package com.example.data.repositories
+
+import com.example.data.remote.sources.RemoteDataSourceImpl
+import com.example.domain.entities.states.Status
+import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class GameDetailsRepositoryImplTest {
+    private val remoteDataSource = RemoteDataSourceImpl()
+
+    @Test
+    fun `getGameDetails success status`() {
+        when (val list = runBlocking { remoteDataSource.getCreators() }) {
+            is Status.Success -> {
+                TestCase.assertNotNull(list.data)
+                println(list.data)
+            }
+            is Status.Failure -> {
+                TestCase.assertNotNull(list.message)
+                println(list.message)
+            }
+            is Status.Loading -> {}
+        }
+    }
+}
