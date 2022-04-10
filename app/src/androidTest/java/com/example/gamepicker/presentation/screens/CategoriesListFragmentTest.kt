896ab10c2950a74669484da97a8374309878a959
IlyaBorisovDly <76851543+IlyaBorisovDly@users.noreply.github.com>
2022-04-10 21:41:57 +0300

    Categorises list fragment tests implemented

--- app/src/androidTest/java/com/example/gamepicker/presentation/screens/CategoriesListFragmentTest.kt
+++ app/src/androidTest/java/com/example/gamepicker/presentation/screens/CategoriesListFragmentTest.kt
@@ -1,5 +1,46 @@
 package com.example.gamepicker.presentation.screens
 
-import junit.framework.TestCase
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.example.gamepicker.R
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
 
-class CategoriesListFragmentTest : TestCase()
+class CategoriesListFragmentTest {
+
+    private lateinit var scenario: FragmentScenario<CategoriesListFragment>
+
+    @Before
+    fun initScenario() {
+        scenario = launchFragmentInContainer(themeResId = R.style.Theme_GamePicker)
+    }
+
+    @Test
+    fun fragmentShouldHaveItsOwnContext() {
+        scenario.onFragment { fragment ->
+            Assert.assertNotNull(fragment.context)
+        }
+    }
+
+    @Test
+    fun categoriesViewShouldHaveProperStringReference() {
+        onView(
+            withId(R.id.textViewCategoriesHeader)
+        ).check(
+            matches(
+                withText(R.string.categories)
+            )
+        )
+    }
+
+    @After
+    fun finishScenario() {
+        scenario.close()
+    }
+}
--- data/src/main/java/com/example/data/remote/api/RetrofitInstance.kt
+++ data/src/main/java/com/example/data/remote/api/RetrofitInstance.kt
@@ -1,17 +1,17 @@
 package com.example.data.remote.api
 
-import com.example.data.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 private const val BASE_URL = "https://api.rawg.io/api/"
+private const val KEY = "152cd61095e34fd18852f0f33626b9c4"
 
 object RetrofitInstance {
 
     private val okHttpClient by lazy {
         OkHttpClient.Builder()
-            .addInterceptor(RawgInterceptor(BuildConfig.RAWG_API_KEY))
+            .addInterceptor(RawgInterceptor(KEY))
             .build()
     }
 
